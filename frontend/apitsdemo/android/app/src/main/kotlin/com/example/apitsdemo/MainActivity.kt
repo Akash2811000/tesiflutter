5c7716bdfd1143109c8190940a9cdf87dbf5c012
Akash2811000 <akashgupta7383@gmail.com>
2022-08-04 16:40:45 +0530

    k

---
+++ frontend/apitsdemo/android/app/src/main/kotlin/com/example/apitsdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apitsdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ frontend/apitsdemo/lib/fetures/apitsdemo/presentation/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("App Name"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  const Color(0xFF3366FF),
+                  const Color(0xFF00CCFF),
+                ],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+          child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("demo"),
+                                    Text("data")
+                                  ],
+                                ),
+                              ),
+                              IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                              IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+                            ],
+
+                          ),
+
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue,
+          onPressed: () {
+
+          },
+        )
+    );
+  }
+}
